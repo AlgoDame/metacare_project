4310a594c1d7846e083bb08e4b2e2f428b3a0618
Chiamaka Ojiyi <chiamakaojiyi@gmail.com>
2022-01-20 18:14:37 +0100

    remove old migration and add cmds to packagejson

--- prisma/migrations/20220119035402_ip_address_update/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `ip_address` to the `CommentModel` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "CommentModel" ADD COLUMN     "ip_address" TEXT NOT NULL;
--- prisma/migrations/20220120170205_init/migration.sql
+++ prisma/migrations/20220120170205_init/migration.sql
@@ -2,6 +2,7 @@
 CREATE TABLE "CommentModel" (
     "id" TEXT NOT NULL,
     "episode_id" INTEGER NOT NULL,
+    "author" TEXT NOT NULL,
     "comment" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
